d2ec13b3d6a5a04767b199d859a128feb4553941
elena-brd <elenarosso11@gmail.com>
2023-09-12 13:28:49 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let halloweenCounter = new Date('10, 31, 00:00:00').getTime();
+// console.log(halloweenCounter);
+
+let timeNow = new Date().getTime();
+let counter = timeNow - halloweenCounter;
+
+let days = Math.floor(timeNow / (1000 * 60 * 60 * 24));
+let hrs = Math.floor(tieNow  ())
+
+
+
